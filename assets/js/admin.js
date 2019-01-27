04b89dbefdc3b7d9e740b94576ab7c9299f9b97b
ever <onliine.comphonia@gmail.com>
2019-01-27 04:47:05 -0600

    Made admin skeleton, added index and add menu page

---
+++ assets/js/admin.js
@@ -0,0 +1,53 @@
+//Menu bar Actions
+$('#menu-action').click(function () {
+    $('.sidebar').toggleClass('active');
+    $('.main').toggleClass('active');
+    $(this).toggleClass('active');
+
+    if ($('.sidebar').hasClass('active')) {
+        $(this).find('i').addClass('fa-close');
+        $(this).find('i').removeClass('fa-bars');
+    } else {
+        $(this).find('i').addClass('fa-bars');
+        $(this).find('i').removeClass('fa-close');
+    }
+});
+
+// Add hover feedback on menu
+$('#menu-action').hover(function () {
+    $('.sidebar').toggleClass('hovered');
+});
+
+//Handle Upload
+function readURL(input) {
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            $('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
