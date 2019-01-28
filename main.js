1440c7c8035706e69e2387b2e14fba6ce2eea6b1
Kishan <kkpatel911@gmail.com>
2019-01-27 21:25:08 -0500

    new home page

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-    $('.slides').slick({
-      fade: true,
-      autoplay: true,
-      autoplaySpeed: 20000,
-      arrow: false,
-      dots: true
-    })
+$('.hero-text').hide();
+$('.hero-text').fadeIn(2000);
+
+// Scroll to specific values
+// scrollTo is the same
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: 'smooth'
+});
+
+// Scroll to a certain element
+document.querySelector('.ourTop').scrollIntoView({
+  behavior: 'smooth'
+});
